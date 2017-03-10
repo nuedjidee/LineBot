7fc0c82a26ef8c6dfc1a3274f10980b4173dab9e
nong00 <nu.ed@hotmail.com>
2017-03-10 23:30:28 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
-$access_token = 'JvXW9ZR3h2C1gw0iS00s4T7tgphrcfqnye6wAln1Yh/RWEsE+2G6VQt+DKvhQtRW+lp/c8r+QHSeOxHZSAbOMvLKGog0msjNu4pzlTS4ILjeZDT3hN7ZZcWQ0zTHl/Sq7gUNzr08Vg+0r+GxtUOgggdB04t89/1O/w1cDnyilFU=';
-$proxy = 'http://fixie:PB77FHxUYoYivES@velodrome.usefixie.com:80';
-$proxyauth = 'nu.ed@hotmail.com:wy*8ghhe';
+$access_token = 'yourToken';
+$proxy = 'Proxy with fixie';
+$proxyauth = 'USER:PASSWORD';
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
