07d2a25e67fa86f751b1b10a7889daee093f85be
nong00 <nu.ed@hotmail.com>
2017-03-10 18:58:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,7 @@
 <?php
 $access_token = 'JvXW9ZR3h2C1gw0iS00s4T7tgphrcfqnye6wAln1Yh/RWEsE+2G6VQt+DKvhQtRW+lp/c8r+QHSeOxHZSAbOMvLKGog0msjNu4pzlTS4ILjeZDT3hN7ZZcWQ0zTHl/Sq7gUNzr08Vg+0r+GxtUOgggdB04t89/1O/w1cDnyilFU=';
-
+$proxy = 'http://fixie:PB77FHxUYoYivES@velodrome.usefixie.com:80';
+$proxyauth = 'nu.ed@hotmail.com:wy*8ghhe';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -37,6 +38,9 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
